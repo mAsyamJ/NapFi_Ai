5a3bc706b925a0af8e9621685196fb42fa672010
mAsyamJ <jayanegara.asyam@gmail.com>
2025-05-07 12:32:06 +0700

    Save changes before running codemod

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  experimental: {
+    appDir: true, // Enable the app directory
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
